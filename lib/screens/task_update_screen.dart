6f82453044242265b789d20bc78b39f3cd91906a
Abid <abidroid1@gmail.com>
2024-03-21 15:50:45 +0500

    task update screen

--- lib/screens/task_update_screen.dart
+++ lib/screens/task_update_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class TaskUpdateScreen extends StatefulWidget {
   const TaskUpdateScreen({super.key});
@@ -8,13 +9,32 @@ class TaskUpdateScreen extends StatefulWidget {
 }
 
 class _TaskUpdateScreenState extends State<TaskUpdateScreen> {
+  TextEditingController taskNameC = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Update Task Screen'),
       ),
-      body: const Placeholder(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(children: [
+          TextField(
+            controller: taskNameC,
+            decoration: const InputDecoration(
+                hintText: 'Task Name',
+                prefixIcon: Icon(Icons.email),
+                border: OutlineInputBorder()
+            ),
+          ),
+
+          const Gap(16),
+          ElevatedButton(onPressed: (){
+
+            }, child: const Text('Update')),
+        ],),
+      ),
     );
   }
 }
