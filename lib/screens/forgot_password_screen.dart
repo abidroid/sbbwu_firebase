8d79b56614cc9cd0e4ea157519a2d351254e5546
Abid <abidroid1@gmail.com>
2024-03-21 15:31:17 +0500

    forgot password screen done

--- lib/screens/forgot_password_screen.dart
+++ lib/screens/forgot_password_screen.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class ForgotPasswordScreen extends StatefulWidget {
   const ForgotPasswordScreen({super.key});
@@ -9,13 +10,39 @@ class ForgotPasswordScreen extends StatefulWidget {
 }
 
 class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
+  TextEditingController emailC = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Forgot Password Screen'),
       ),
-      body: const Placeholder(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(children: [
+          const Gap(16),
+
+          const Text('Please provide your email.\nWe will send an email for password reset',
+          textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 20),
+          ),
+
+          const Gap(16),
+
+          TextField(
+            controller: emailC,
+            decoration: const InputDecoration(
+                hintText: 'Email',
+                prefixIcon: Icon(Icons.email),
+                border: OutlineInputBorder()),
+          ),
+          const Gap(16),
+
+          ElevatedButton(onPressed: (){}, child: const Text('Submit')),
+
+        ],),
+      ),
     );
   }
 }
