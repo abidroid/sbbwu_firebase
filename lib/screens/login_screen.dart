f4d0341e2726bb78be201cb4640728a39cb0215a
Abid <abidroid1@gmail.com>
2024-03-25 15:43:32 +0500

    login done

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:gap/gap.dart';
 import 'package:sbbwu_firebase/screens/dashboard_screen.dart';
 import 'package:sbbwu_firebase/screens/forgot_password_screen.dart';
@@ -69,10 +71,49 @@ class _LoginScreenState extends State<LoginScreen> {
               style: TextStyle(fontSize: 12, fontStyle: FontStyle.italic),)),
 
 
-            ElevatedButton(onPressed: (){
-              Navigator.of(context).push(MaterialPageRoute(builder: (context){
-                return const DashboardScreen();
-              }));
+            ElevatedButton(onPressed: () async {
+
+              String email = emailC.text.trim();
+              if( email.isEmpty){
+                Fluttertoast.showToast( msg: 'Please provide email');
+                return;
+              }
+              String password = passC.text.trim();
+              if( password.isEmpty){
+                Fluttertoast.showToast( msg: 'Please provide password');
+                return;
+              }
+
+              FirebaseAuth auth = FirebaseAuth.instance;
+
+              try{
+                UserCredential userCredentials = await auth.signInWithEmailAndPassword(email: email, password: password);
+
+                if( userCredentials.user != null ){
+
+                  // check if email is verified
+
+                  if( userCredentials.user!.emailVerified){
+                    Navigator.of(context).push(MaterialPageRoute(builder: (context){
+                      return const DashboardScreen();
+                    }));
+                  }else{
+
+                    Navigator.of(context).push(MaterialPageRoute(builder: (context){
+                      return const VerifyEmailScreen();
+                    }));
+                  }
+
+
+                }
+
+
+              }on FirebaseAuthException catch (e){
+                Fluttertoast.showToast(msg: e.message ?? '');
+              }
+
+
+
 
 
             }, child: const Text('Login')),
