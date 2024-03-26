23efdbf87ae4405de43b4057a408cd9b28ed29f6
Abid <abidroid1@gmail.com>
2024-03-26 16:49:04 +0500

    password reset email done

--- lib/screens/forgot_password_screen.dart
+++ lib/screens/forgot_password_screen.dart
@@ -1,5 +1,7 @@
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:gap/gap.dart';
 
 class ForgotPasswordScreen extends StatefulWidget {
@@ -39,7 +41,24 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
           ),
           const Gap(16),
 
-          ElevatedButton(onPressed: (){}, child: const Text('Submit')),
+          ElevatedButton(onPressed: () async {
+
+            String email = emailC.text.trim();
+
+            if( email.isNotEmpty){
+
+
+              try{
+                await FirebaseAuth.instance.sendPasswordResetEmail(email: email);
+
+                Fluttertoast.showToast(msg: 'Email Sent', fontSize: 30);
+              }on FirebaseAuthException catch (e){
+                Fluttertoast.showToast(msg: e.message!, fontSize: 30);
+              }
+            }else{
+              Fluttertoast.showToast(msg: "please provide email");
+            }
+          }, child: const Text('Submit')),
 
         ],),
       ),
